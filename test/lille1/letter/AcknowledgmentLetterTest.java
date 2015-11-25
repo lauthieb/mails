57dae830458d29afa7a357f4a9be193062a1ca8c
crastchet <crastchet@gmail.com>
2015-11-26 00:19:35 +0100

    Ajout de tests pour AcknowledgmentLetter. Il n'y a qu'un test qui teste le toString

---
+++ test/lille1/letter/AcknowledgmentLetterTest.java
@@ -0,0 +1,20 @@
+package lille1.letter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AcknowledgmentLetterTest extends SimpleLetterTest {
+
+	@Override
+	public void createLetter() {
+		super.createLetter();
+		letter = new AcknowledgmentLetter("Je suis un accusé de réception dans la classe AcknowledgmentLetterTest", sender, receiver);
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals("an aknowledgment of receipt which is a simple letter whose content is a text content (Je suis un accusé de réception dans la classe AcknowledgmentLetterTest)", letter.toString());
+	}
+
+}
